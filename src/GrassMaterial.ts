0541084002a13c23239a360810b213c3b9c56614
Your Name <rahulmohature0421@gmail.com>
2024-08-03 16:07:16 +0530

    new code addded

--- src/GrassMaterial.ts
+++ src/GrassMaterial.ts
@@ -8,4 +8,9 @@ interface GrassUniformInterface{
     uShadowDarkness?: { value: number }
     uGrassLightIntensity?: { value: number }
     uNoiseScale?: { value: number}
+    uPlayerPosition?: { value:THREE.Vector3}
+    baseColor?: { value: THREE.Color}
+    tipColor1?: { value: THREE.Color}
+    tipColor2?: { value: THREE.Color}
+    
 }
